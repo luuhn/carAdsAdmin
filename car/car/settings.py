3ea4c6d2a45cf4cc2c4d4fd680cd4319ad6c2040
Ha Ngoc Luu <hangocluu@Has-MacBook-Pro.local>
2019-11-27 14:35:41 +0700

    change setting

--- car/car/settings.py
+++ car/car/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3kgi19n@#-va$(=34*t2sx1t_ho2ve14_*2+br%fq1t1x^-j5q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.209.98.165']
 
 
 # Application definition
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'car.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/root/my.cnf',
+        },
     }
 }
 
